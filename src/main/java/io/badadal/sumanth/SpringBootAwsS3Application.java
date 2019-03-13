dc3a2f6d8f90dee43c2fefb9f794fdb5276f4f50
Badadal, Sumanth S <sbadadal@jcp.com>
2019-03-13 11:01:18 +0530

    Initial commit

---
+++ src/main/java/io/badadal/sumanth/SpringBootAwsS3Application.java
@@ -0,0 +1,12 @@
+package io.badadal.sumanth;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootAwsS3Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootAwsS3Application.class, args);
+	}
+}
